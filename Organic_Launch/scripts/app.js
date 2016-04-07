b83efe6e11dd2237518eba6d9af880e2fa558539
Marrion Luaka <marrion91300@gmail.com>
2016-04-07 14:57:23 -0700

    app.js

--- Organic_Launch/scripts/app.js
+++ Organic_Launch/scripts/app.js
@@ -2,3 +2,16 @@
     speed: 900,
     fluid: true
 });
+
+//------------------NAVBAR
+
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
